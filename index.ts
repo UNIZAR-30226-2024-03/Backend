5ac1919d271b2f8bdce0ed3e368b9a4198b0024e
daniihc16 <danihercecruz@gmail.com>
2024-03-01 00:09:07 +0100

    Estructura de ficheros de rutas y ejemplo de rutas

--- index.ts
+++ index.ts
@@ -2,14 +2,22 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
-dotenv.config();
+import { routes } from "./routes/routesExport";
+
+import bodyParser from 'body-parser';
+
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+// body-parser
+app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+dotenv.config();
+
+// Routes
+app.use(routes);
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
