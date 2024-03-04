4dd1712725fc8acb43b479c6b870741fc9f4835c
alvaro <alvaro.defrancisco03@gmail.com>
2024-03-04 21:30:28 +0100

    borrado sequelize & revertido atributo google

---
+++ prisma/migrations/20240304203003_ggle_out/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `googleLogin` on the `Usuario` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Usuario" DROP COLUMN "googleLogin";
