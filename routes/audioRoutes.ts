7f37b83cfb420e60b5e18765913354b07054806d
849183 <849183@unizar.es>
2024-03-07 12:19:01 +0100

    Some changes

--- routes/audioRoutes.ts
+++ routes/audioRoutes.ts
@@ -1,11 +1,11 @@
 // -AUDIO ----> Alain
-// ------[POST]/audio/upload : Sube un audio a la base de datos.
-// ------[DELETE]/audio/<idAudio>/ : Borra un audio de la base de datos.
+// ------/audio/delete/<idAudio> : Borra un audio de la base de datos.
 // ------[PUT]/audio/<idAudio>/ : Edita un audio de la base de datos.
-// ------/audio/<idAudio>/ : Devuelve la información de un audio (autor y etiquetas incluidos)
-// ------/audio/play/<idAudio>/ : Devuelve el audio a reproducir
 
 import express, { Express, Request, Response } from "express";
+
+import { PrismaClient } from '@prisma/client';
+
 
 // Librearía para subir archivos de audio
 import multer from 'multer';
@@ -17,6 +17,7 @@ interface MulterRequest extends Request {
     file?: Express.Multer.File; // file puede ser undefined si no se sube ningún archivo
 }
 
+const prisma = new PrismaClient();
 const projectRootPath = process.cwd(); // Devuelve el directorio raíz del proyecto y se almacena en una constante
 var path = require('path'); //Variable para manejar rutas de archivos
 var fs = require('fs'); //Variable para manejar archivos, leer, escribir, etc
@@ -44,10 +45,22 @@ var upload = multer(
 
 //PRE: Se recibe un id de audio correcto en la URL
 //POST: Sube obtiene información de un audio con formato JSON
-audioRouter.get('/audio/:idaudio', function(req, res: Response) {
-     /*
-        Lógica de hacer el select en la Base de datos
-    */
+audioRouter.get('/audio/:idaudio', async function(req, res: Response) {
+    const id = Number(req.params.idaudio);
+    try {
+        const audio = await prisma.audio.findUnique({
+            where: {
+                idAudio: id,
+            },
+        });
+        if (audio) {
+            res.json(audio);
+        } else {
+            return res.json({ Error: '1',message:'Error, audio no encontrado en la base de datos'})
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 //PRE: Se recibe un id de audio correcto en la URL
@@ -60,7 +73,76 @@ audioRouter.get('/audio/play/:idaudio', function(req, res) {
 
 //PRE: Se recibe un audio en formato .mp3 o .wav
 //POST: Se sube el archivo a la base de datos
-audioRouter.post('/audio/upload', upload.single('cancion'), function(req: MulterRequest, res: Response) {
+audioRouter.post('/audio/upload', upload.single('cancion'), async function(req: MulterRequest, res: Response) {
+    try {
+        const audioData = {
+            titulo: req.body.titulo,
+            path: req.body.path,
+            duracionSeg: req.body.duracionSeg,
+            fechaLanz: req.body.fechaLanz,
+            esAlbum: req.body.esAlbum,
+        };
+
+        console.log(audioData);
+        const audio = await prisma.audio.create({
+            data: audioData,
+        });
+
+        res.json( { message: 'Audio added successfully' } );
+    } catch (error) {
+        res.status(500).send(error);
+    }
 
 
+});
+
+
+//PRE: Se recibe un id de audio correcto en la URL
+//POST: Se elimina el registro de BBDD y la canción del servidor
+audioRouter.get('/audio/delete/:idaudio', async function(req, res: Response) {
+    const id = Number(req.params.idaudio);
+
+    try {
+
+        const audioRuta = await prisma.audio.findUnique({
+            where: {
+                idAudio: id,
+            },
+        });
+        if (!audioRuta) {
+            return res.json({ Error: '1',message:'Error, audio no encontrado en la base de datos'})
+        }
+        const audio = await prisma.audio.delete({
+            where: {
+                idAudio: id,
+            },
+        });
+        try{
+            fs.unlinkSync(path.join(projectRootPath,audioRuta.path)); //Borra el archivo del servidor
+        }
+        catch (error){
+            res.json({ Error: 'Error, audio no encontrado en el servidor local' });
+        }
+        res.json({ message: 'Audio deleted successfully' });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+
+//PRE: Se recibe un id de audio correcto en la URL
+//POST: Se edita el registro de BBDD y la canción del servidor
+audioRouter.put('/audio/:idaudio', upload.single('cancion'), async function(req: MulterRequest, res: Response) {
+    const id = Number(req.params.idaudio);
+    try {
+        const audio = await prisma.audio.update({
+            where: {
+                idAudio: id,
+            },
+            data: req.body,
+        });
+        res.json( { message: 'Audio updated successfully' } );
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
