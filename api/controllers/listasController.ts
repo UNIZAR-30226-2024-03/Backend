1afab39cfbe4969f6a4d84fdc091516815e404f0
daniihc16 <danihercecruz@gmail.com>
2024-03-17 20:17:23 +0100

    Avance listas controllers

--- api/controllers/listasController.ts
+++ api/controllers/listasController.ts
@@ -1,8 +1,10 @@
-/* import httpStatus from 'http-status';
+import httpStatus from 'http-status';
 import pick from '../utils/errorHandling/utils/pick';
 import ApiError from '../utils/errorHandling/utils/ApiError';
 import catchAsync from '../utils/errorHandling/utils/catchAsync';
 import { Request, Response } from 'express';
+import * as listasDb from '../../db/listaDb';
+
 
 
 
@@ -10,39 +12,81 @@ import { Request, Response } from 'express';
 //[DELETE]/lista/<idLista>/ : Borra una lista.
 //[PUT]/lista/<idLista>/ : Edita una lista.
 //[GET]/lista/<idLista>/ : Devuelve la información de una lista (audios que contiene incluidos)
-///lista/follow/<idLista>/<idUsuario> : Añade la lista a las seguidas por el usuario.
-///lista/unfollow/<idLista>/<idUsuario> : Elimina la lista de las seguidas por el usuario.
+//[POST]/lista/follow/<idLista>/<idUsuario> : Añade la lista a las seguidas por el usuario.
+//[DELETE]/lista/follow/<idLista>/<idUsuario> : Elimina la lista de las seguidas por el usuario.
 //[POST]/lista/audio/<idLista>/<idAudio> : Añade un audio a la lista.
 //[DELETE]/lista/audio/<idLista>/<idAudio> : Elimina un audio de la lista.
 ///lista/add-collaborator/<idLista>/<idUsuario>/: Añade un colaborador a la lista.
 
+
+/**
+ * Crea una lista nueva
+  * @param {ObjectId} idUsuario
+  * @param {string} nombre
+  * @param {string} descripcion
+  * @param {boolean} esPrivada
+  * @param {string} img
+  * @param {boolean} esAlbum
+  * @param {TipoLista} tipoLista
+  * @param {number[]} audios
+  * @returns {Promise<Lista>}
+ */
 export const createLista = catchAsync(async (req : Request, res : Response) => {
-  const { nombre, descripcion, esPrivada, fechaUltimaMod, img, esAlbum, tipo, propietarioCreador, audios } = req.body; // No se necesita Seguidores porque al crear una lista no tiene seguidores
-  const user = await listaService.createLista(nombre, descripcion, esPrivada, fechaUltimaMod, img, esAlbum, tipo, propietarioCreador, audios);
-  res.status(httpStatus.CREATED).send(user);
+  const { nombre, descripcion, esPrivada, img, esAlbum, tipoLista, idUsuario, audios } = req.body; 
+  // No se necesita Seguidores porque al crear una lista no tiene seguidores
+  // No necesita fechaUltimaMod porque se crea en el momento de la creación
+  const lista = await listasDb.createLista(nombre, descripcion, esPrivada, img, esAlbum, tipoLista, idUsuario, audios);
+  res.status(httpStatus.CREATED).send(lista);
 });
 
-export const getUsers = catchAsync(async (req : Request, res : Response) => {
-  const filter = pick(req.query, ['name', 'role']); // Ponemos todos los atributos que permitimos filtrar
-  const options = pick(req.query, ['sortBy', 'limit', 'page']); 
-  const result = await listaService.queryUsers(filter, options);
-  res.send(result);
-});
 
-export const getUser = catchAsync(async (req : Request, res : Response) => {
-  const user = await listaService.getUserById(req.params.userId);
-  if (!user) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
-  }
-  res.send(user);
-});
-
-export const updateUser = catchAsync(async (req : Request, res : Response) => {
-  const user = await listaService.updateUserById(req.params.userId, req.body);
-  res.send(user);
-});
-
-export const deleteUser = catchAsync(async (req : Request, res : Response) => {
-  await listaService.deleteUserById(req.params.userId);
+/**
+ * Elimina una lista
+ * @param {ObjectId} id
+ * @returns {Promise<Lista>}
+ */
+export const deleteLista = catchAsync(async (req : Request, res : Response) => {
+  await listasDb.deleteListaById(parseInt(req.params.ListaId));
   res.status(httpStatus.NO_CONTENT).send();
-}); */
+});
+
+
+/**
+ * Edita una lista
+ * @param {ObjectId} id
+ * @param {Object} updateBody Es un objeto de la forma: { nombre, descripcion, esPrivada, imgLista, tipoLista, audios }
+ * @returns {Promise<Lista>}
+ * @throws {ApiError}
+ */
+export const updateLista = catchAsync(async (req : Request, res : Response) => {
+  const lista = await listasDb.updateListaById(parseInt(req.params.ListaId), req.body);
+  res.send(lista);
+});
+
+
+/**
+ * Devuelve la lista con el id dado
+ * @param {ObjectId} id
+ * @returns {Promise<Lista | null>}
+ * @throws {ApiError}
+ */
+export const getListaById = catchAsync(async (req : Request, res : Response) => {
+  const lista = await listasDb.getListaById(parseInt(req.params.ListaId));
+  if (!lista) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Lista not found');
+  }
+  res.send(lista);
+});
+
+
+/**
+ * Añade la lista a las seguidas por el usuario.
+ * @param {ObjectId} idLista
+ * @param {ObjectId} idUsuario
+ * @returns {Promise<Lista>}
+ */
+export const followLista = catchAsync(async (req : Request, res : Response) => {
+  const lista = await listasDb.followLista(parseInt(req.params.ListaId), parseInt(req.params.UserId));
+  res.send(lista);
+});
+
---
+++ db/listaDb.ts
@@ -0,0 +1,109 @@
+import { Lista, Prisma, TipoLista } from '@prisma/client';
+import httpStatus from 'http-status';
+import prisma from '../prisma/client.js';
+import ApiError from '../api/utils/errorHandling/utils/ApiError.js';
+// import { encryptPassword } from '../utils/encryption';
+
+/**
+ * Crea una lista nueva
+ * @param {Object} listaBody
+ * @returns {Promise<Lista>}
+ */
+export const createLista = async (
+    nombre: string,
+    esAlbum: boolean,
+    esPrivada: boolean,
+    idCreador: number,
+    descripcion: string = '',
+    imgLista: string| null = process.env.DEFAULT_PLAYLIST_PICTURE || "",
+    tipoLista: TipoLista,
+    audios: number[]
+): Promise<Lista> => {
+    // Permitimos que un usuario tenga varias listas que se llamen igual
+    // Esto se debe a que puede añadir un segundo propietario tras crear la lista y que esto 
+    // de problemas con el nombre de la lista
+  // if (await getListaByName(nombre)) {
+  //   throw new ApiError(httpStatus.BAD_REQUEST, 'Name already taken');
+  // }
+  return prisma.lista.create({
+    data: {
+        nombre,
+        esAlbum,
+        esPrivada,
+        Propietarios: {
+            connect: { idUsuario: idCreador }
+        },
+        descripcion,
+        fechaUltimaMod: new Date(), // new Date() devuelve la fecha actual
+        imgLista,
+        tipoLista,
+        Audios: {
+          // mapeamos los ids de los audios a un array de objetos con el id
+          // para que al pasarlo a connect se seleccionen los audios con esos ids
+          connect: audios.map((idAudio) => ({ idAudio })),
+        }
+    }
+  });
+};
+
+
+/**
+ * Devuelve la lista con el id dado
+ * @param {ObjectId} id
+ * @returns {Promise<Lista | null>}
+ */
+export const getListaById = async (id: number): Promise<Lista | null> => {
+  return prisma.lista.findUnique({
+    where: { idLista: id }
+  });
+}
+
+
+/**
+ * Edita una lista
+ * @param {ObjectId} id
+ * @param {Object} updateBody Es un objeto de la forma
+ * @returns {Promise<Lista>}
+ * @throws {ApiError}
+ */
+export const updateListaById = async (
+  id: number,
+  updateBody: Prisma.ListaUpdateInput
+): Promise<Lista> => {
+  const lista = await getListaById(id);
+  if (!lista) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Lista not found');
+  }
+  return prisma.lista.update({
+    where: { idLista: lista.idLista },
+    data: updateBody
+  });
+};
+
+
+
+/**
+ * Elimina una lista
+ * @param {ObjectId} id
+ * @returns {Promise<Lista>}
+ * @throws {ApiError}
+ */
+export const deleteListaById = async (id: number): Promise<Lista> => {
+  const lista = await getListaById(id);
+  if (!lista) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Lista not found');
+  }
+  await prisma.lista.delete({ where: { idLista: lista.idLista } });
+  return lista;
+}
+
+
+
+
+
+
+
+
+
+
+
