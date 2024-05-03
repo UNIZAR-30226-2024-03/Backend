5f0044c5dd52d23fd7a2f47f9bbfc7447c334026
Garicore01 <848905@unizar.es>
2024-05-03 18:20:06 +0200

    Update PLSQL functions

--- sql/functions.sql
+++ sql/functions.sql
@@ -8,10 +8,9 @@ BEGIN
     -- Crear listas de reproducción basadas en géneros
     FOR genero IN SELECT nombre FROM public."EtiquetaCancion" LOOP
         -- Generar nombre de lista basado en el género
-        lista_genero_nombre := 'Top ' || genero.nombre || ' Songs';
+        lista_genero_nombre := 'Top ' || genero.nombre;
 
         -- Insertar lista de reproducción si no existe
-        -- Utilizamos PERFORM para ejecutar la función sin necesidad de retornar un valor.
         PERFORM crear_lista_si_no_existe(lista_genero_nombre);
        
         -- Insertar audios del género más escuchados en la lista de reproducción
@@ -27,27 +26,61 @@ BEGIN
         	SELECT "idEtiqueta" 
         	FROM public."EtiquetaCancion" 
         	WHERE nombre = genero.nombre
+        ) AND aec."A" = (
+            SELECT "idAudio"
+            FROM public."Audio"
+            WHERE "esPrivada" = false
         )
         GROUP BY aec."A"
         ORDER BY COUNT(*) DESC
-        LIMIT 50;
+        LIMIT 10;
+
+    END LOOP;
+   
+   
+   FOR genero IN SELECT nombre FROM public."EtiquetaPodcast" LOOP
+        -- Generar nombre de lista basado en el género
+        lista_genero_nombre := 'Top ' || genero.nombre;
+
+        -- Insertar lista de reproducción si no existe
+        PERFORM crear_lista_si_no_existe(lista_genero_nombre);
+       
+        -- Insertar audios del género más escuchados en la lista de reproducción
+        INSERT INTO public."_AudioToLista" ("A", "B")
+        SELECT aep."A", (
+        	SELECT "idLista" 
+        	FROM public."Lista" 
+        	WHERE nombre = lista_genero_nombre
+        )
+        FROM public."_AudioToEtiquetaPodcast" AS aep
+        	INNER JOIN public."Escucha" AS e ON aep."A" = e."idAudio" 
+        WHERE aep."B" = (
+        	SELECT "idEtiqueta" 
+        	FROM public."EtiquetaCancion" 
+        	WHERE nombre = genero.nombre
+        )
+        GROUP BY aec."A"
+        ORDER BY COUNT(*) DESC
+        LIMIT 10;
 
     END LOOP;
 
 	-- Crear lista de reproducción basada en los audios más escuchados
+    -- Comprobar que son públicas.
 	FOR audio_mas_escuchado IN SELECT "idAudio" FROM (
 	    SELECT "idAudio", COUNT(*) as escuchas
-	    FROM public."Escucha"
+	    FROM public."Escucha" AS e
+            INNER JOIN public."Audios" AS a ON e."idAudio" = a."idAudio"
+        WHERE e."idAudio" = a."idAudio" AND a."esPrivada" = false        
 	    GROUP BY "idAudio"
 	    ORDER BY escuchas DESC
-	    LIMIT 50
+	    LIMIT 10
 	) AS subquery LOOP
 	    -- Generar nombre de lista
-	    lista_top_nombre := 'Top 10 Most Played';
+	    lista_top_nombre := 'Top 10 Global';
 	
 	    -- Insertar lista de reproducción si no existe
-        -- Utilizamos PERFORM para ejecutar la función sin necesidad de retornar un valor.
-	    PERFORM crear_lista_si_no_existe(lista_top_nombre); 
+	    PERFORM crear_lista_si_no_existe(lista_top_nombre);
 	
 	    -- Insertar audios más escuchados en la lista de reproducción
 	    INSERT INTO public."_AudioToLista" ("A", "B")
@@ -63,20 +96,35 @@ $$ LANGUAGE plpgsql;
 
 
 
--- #################################################################################################
+
+
+-- Por defecto las listas son publicas. ¡ Añadir propiertario e imagen, fecha !
 CREATE OR REPLACE FUNCTION crear_lista_si_no_existe(nombre_lista TEXT) RETURNS VOID AS $$
 BEGIN
-	INSERT INTO public."Lista" (nombre, "tipoLista")
-	    SELECT nombre_lista, 'NORMAL'
+	INSERT INTO public."Lista" (nombre, "tipoLista") 
+	    SELECT nombre_lista, 'NORMAL' -- Comprobar que tipo de lista es
 	    WHERE NOT EXISTS (
 	        SELECT 1 
 	        FROM public."Lista" 
 	        WHERE nombre = nombre_lista
 	    );
 END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;	
 
--- #################################################################################################
+
+
+
+select generar_listas_reproduccion();
+
+
+
+
+
+
+
+
+-- Función que se desencadena con la llamada de un trigger y válida si la asignación de etiquetas
+-- es correcta o no.
 CREATE OR REPLACE FUNCTION validar_tipo_audio()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -100,3 +148,6 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
