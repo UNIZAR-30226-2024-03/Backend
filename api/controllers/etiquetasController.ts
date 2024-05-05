ac03ab1f875d6bcca51198d9290d1a0b3e6b6362
daniihc16 <danihercecruz@gmail.com>
2024-05-05 17:33:01 +0200

    Ya funciona /etiquetas/infinite

--- api/controllers/etiquetasController.ts
+++ api/controllers/etiquetasController.ts
@@ -94,6 +94,7 @@ export const tagsNUltimasEscuchas = async (req: Request, res: Response) => {
     const etiquetas = await etiquetasDbJs.tagsNLastListened(idUsuario, nAudios);
 
     res.json(etiquetas);
+    // res.status(httpStatus.NOT_IMPLEMENTED).json({ message: "Not implemented" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error obteniendo etiquetas de las últimas escuchas" });
@@ -110,9 +111,9 @@ export const getNAudiosByTags = async (req: Request, res: Response) => {
     const nAudiosToGetTagsFrom = Number(req.params.nAudiosToGetTagsFrom);
     const nAudios = Number(req.params.nAudiosResult);
 
-    console.log("idUsuario: ", idUsuario);
-    console.log("nAudiosToGetTagsFrom: ", nAudiosToGetTagsFrom);
-    console.log("nAudios: ", nAudios);
+    // console.log("idUsuario: ", idUsuario);
+    // console.log("nAudiosToGetTagsFrom: ", nAudiosToGetTagsFrom);
+    // console.log("nAudios: ", nAudios);
 
     if (!nAudiosToGetTagsFrom || nAudiosToGetTagsFrom <= 0) {
       return res.status(400).json({ message: 'Bad request. numEscuchas must be a positive number' });
@@ -122,7 +123,7 @@ export const getNAudiosByTags = async (req: Request, res: Response) => {
     }
 
     const tags = await etiquetasDbJs.tagsNLastListened(idUsuario, nAudiosToGetTagsFrom);
-    console.log(tags);
+    // console.log(tags);
     const audios = await etiquetasDbJs.getNAudiosByTags(nAudios, tags);
 
     res.json(audios);
